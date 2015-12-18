306fc4a51aac9726b45aa8698b9bcfb2b6d33825
Felix Barros <felix.barros@gmail.com>
2015-12-17 19:24:07 -0500

    Remove storyboard and replace with manual view controller / xib.

--- Lecture62-NoStoryboard/AppDelegate.swift
+++ Lecture62-NoStoryboard/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let firstViewController = FirstViewController(nibName: "FirstViewController", bundle: nil)
+        window?.rootViewController = firstViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ Lecture62-NoStoryboard/FirstViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FirstViewController.swift
+//  Lecture62-NoStoryboard
+//
+//  Created by Felix Barros on 12/17/15.
+//  Copyright © 2015 Bits That Matter. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.blueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
